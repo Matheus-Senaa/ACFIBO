fd2cc3240cbe967490ad6a59313274179d96affd
Sena <69823724+Matheus-Senaa@users.noreply.github.com>
2020-10-17 14:45:14 -0300

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -1,9 +1,24 @@
-anterior = 0
-proxima = 1
-soma = 1
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
 
-for n in range (0, 101):
-    print(anterior)
-    soma = proxima + anterior
-    anterior = proxima
-    proxima = soma
+app = Flask(__name__)
+
+@app.route('/')
+def nao_entre_em_panico():
+
+    anterior = 0
+    proxima = 1
+    soma = 1
+
+    for n in range (0, 101):
+        print(anterior)
+        soma = proxima + anterior
+        anterior = proxima
+        proxima = soma
+    
+     return anterior
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
