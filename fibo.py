b77955248a6dd41b4410ed4fa6eb5cd948d50d44
Sena <69823724+Matheus-Senaa@users.noreply.github.com>
2020-10-22 18:31:50 -0300

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -1,25 +1,28 @@
-import os 
+import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
 app = Flask(__name__)
 
 @app.route('/')
-def nao_entre_em_panico():
-    proximo = 1
-    anterior = 0
-    limite = 98
-    found = 0
-    resposta = "1,\n"
-    while (found < limite):
-        tmp = proximo
-        proximo = proximo + anterior
-        anterior = tmp
-        found = found+1
-        resposta+= str(proximo) + ",\n"
-        
-    return resposta
+def sequencia_fibonacci():
+   proximo = 1
+   anterior = 0
+   limite = 100
+   found = 0
+   resposta = "0,"
+   while (found < limite):
+       found=found+1 
+       if (found):
+           tmp = proximo
+           proximo = proximo + anterior
+           anterior = tmp
+           resposta+= str(proximo) + ","
+           if (found % 5 == 0):
+               resposta = resposta + "<br>" 
 
-if _name_ == "_main_":
+   return resposta
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
